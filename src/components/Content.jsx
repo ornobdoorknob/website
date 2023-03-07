a2e4245c4372feb8241ecf1d40d7ce559caa3e57
Tahmid Khan Arnab <arna0016@algonquinlive.com>
2023-03-07 00:50:38 -0500

    ver 10 added nav buttons

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const Content = () => {
     return (
         <div>
-            <div class = "min-h-screen border-b-4 border-cyan-400">
-             <div class="pt-10 font-oswald text-4xl bg-clip-text bg-gradient-to-l to-cyan-400 from-blue-500 text-transparent text-4xl font-extrabold text-center">
+            <div>
+                <a name ="skills"></a>
+             <div name="skill" class="pt-10 font-oswald text-4xl bg-clip-text bg-gradient-to-l to-cyan-400 from-blue-500 text-transparent text-4xl font-extrabold text-center">
                     Skills
             </div>
             <div class="flex items-center justify-center min-h-auto container mx-auto w-full">
@@ -69,12 +70,13 @@ const Content = () => {
                 </div>
             </div>
             </div>
-            <div class = "min-h-screen align-center">
+            <div class = "align-center border-t-4 border-cyan-400">
+            <a name ="projects"></a>
             <div class="pt-10 font-oswald text-4xl bg-clip-text bg-gradient-to-l to-cyan-400 from-blue-500 text-transparent text-4xl font-extrabold text-center">
-                    Projects
+                Projects
             </div>
             <div class="py-10 flex items-center justify-center min-h-auto container mx-auto w-full text-center">
-                <div class="font-poppins grid grid cols-3 md:grid-cols-3 sm: grid-cols-1">
+                <div class="font-poppins grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
                     <div class="my-5 rounded-xl text-white bg-secondary mx-5">
                         <div class="p-5 flex flex-col">
                             <div class="rounded-xl overflow-hidden">
@@ -84,8 +86,8 @@ const Content = () => {
                             <div>
                                 <span class="bg-red-600 text-white text-xs mr-2 px-2.5 py-0.5 rounded-full">Java</span>
                             </div>
-                            <div class="font-poppins">A program capable of initializing an Array and populating it and then utilizing data structures and algorithms to sort and search through the Array and then printing the time taken.</div>
-                            <a href="https://github.com/ornobdoorknob/resume-projects/tree/main/SearchAndSort" class="mt-5 transition-all duration-1000 text-center py-2 rounded-lg bg-primary hover:bg-white hover:text-black">View code</a>
+                            <div class="font-poppins text-xs">A program capable of initializing an Array and populating itm utilizing data structures and algorithms to sort and search through the Array and then printing the time taken.</div>
+                            <a href="https://github.com/ornobdoorknob/resume-projects/tree/main/SearchAndSort" class="mt-5 transition-all duration-300 text-center py-2 rounded-lg bg-primary hover:bg-white hover:text-black">View code</a>
                         </div>
                     </div>
                     <div class="my-5 rounded-xl text-white bg-secondary mx-5">
@@ -97,8 +99,8 @@ const Content = () => {
                             <div>
                                 <span class="bg-blue-600 text-white text-xs mr-2 px-2.5 py-0.5 rounded-full">C</span>
                             </div>
-                            <div class="font-poppins">A program capable of prompting user to input their phone number, and based on the input, adds generic formatting to the number and outputs a report to the user.</div>
-                            <a href="https://github.com/ornobdoorknob/resume-projects/tree/main/PhoneNumberFormatter" class="mt-5 transition-all duration-1000 text-center py-2 rounded-lg bg-primary hover:bg-white hover:text-black">View code</a>
+                            <div class="font-poppins text-xs">A program capable of prompting user to input their phone number, and based on the input, adds generic formatting to the number and outputs a report to the user.</div>
+                            <a href="https://github.com/ornobdoorknob/resume-projects/tree/main/PhoneNumberFormatter" class="mt-5 transition-all duration-300 text-center py-2 rounded-lg bg-primary hover:bg-white hover:text-black">View code</a>
                         </div>
                     </div>
                     <div class="my-5 rounded-xl text-white bg-secondary mx-5 border-white">
@@ -112,8 +114,8 @@ const Content = () => {
                                 <span class="bg-cyan-500 text-white text-xs mr-2 px-2.5 py-0.5 rounded-full">TailwindCSS</span>
                                 <span class="bg-amber-400 text-white text-xs mr-2 px-2.5 py-0.5 rounded-full">ViteJS</span>
                             </div>
-                            <div class="font-poppins">Responsive website built using a mobile-first approach utilizing TailwindCSS for utilies, ReactJS for dynamic components and ViteJS for production optimization.</div>
-                            <a href="https://github.com/ornobdoorknob/website" class="mt-5 transition-all duration-1000 text-center py-2 rounded-lg bg-primary hover:bg-white hover:text-black">View code</a>
+                            <div class="font-poppins text-xs">Responsive website built using a mobile-first approach utilizing TailwindCSS as framework for utilies, ReactJS for dynamic components and ViteJS for production optimization.</div>
+                            <a href="https://github.com/ornobdoorknob/website" class="mt-5 transition-all duration-300 text-center py-2 rounded-lg bg-primary hover:bg-white hover:text-black">View code</a>
                         </div>
                     </div>
                 </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,18 +9,25 @@ const Footer = () => {
                 </a>
                 <ul class="flex flex-wrap items-center mb-6 text-sm sm:mb-0 text-gray-500">
                     <li class="px-3 font-poppins">
-                        <a href="https://www.linkedin.com/in/tahmid-khan-arnab/" class="transition-all duration-500 mr-4 md:mr-6 hover:text-white"><img src="linkedin.png" className='h-6'></img>LinkedIn</a>
+                        <a href="https://www.linkedin.com/in/tahmid-khan-arnab/" class="transition-all duration-300 mr-4 md:mr-6 hover:text-white"><img src="linkedin.png" className='h-6'></img>LinkedIn</a>
                     </li>
                     <li class="px-3 font-poppins">
-                        <a href="https://github.com/ornobdoorknob" class="transition-all duration-500 mr-4 md:mr-6 hover:text-white"><img src="gh.png" className='h-6'></img>GitHub</a>
+                        <a href="https://github.com/ornobdoorknob" class="transition-all duration-300 mr-4 md:mr-6 hover:text-white"><img src="gh.png" className='h-6'></img>GitHub</a>
                     </li>
                     <li class="px-3 font-poppins">
-                        <a href="mailto:arna0016@algonquinlive.com" class="transition-all duration-500 mr-4 md:mr-6 hover:text-white"><img src="email.svg" className='h-6'></img>Email</a>
+                        <a href="mailto:arna0016@algonquinlive.com" class="transition-all duration-300 mr-4 md:mr-6 hover:text-white"><img src="email.svg" className='h-6'></img>Email</a>
+                    </li>
+                    <li class="px-3 font-poppins pt-4">
+                        <a href='#top' class="transition-all duration-300 mr-4 md:mr-6 hover:text-white"> <button class="border-2 border-white relative inline-flex items-center justify-center mb-2 mr-2 overflow-hidden text-lg font-medium text-white rounded-lg group bg-black">
+                            <span class="relative px-2 py-2 transition-all ease-in duration-300 hover:bg-white hover:text-black bg-black">
+                                Back to the top
+                            </span>
+                        </button></a>
                     </li>
                 </ul>
             </div>
             <hr class="my-6 sm:mx-auto border-gray-500 lg:my-8" />
-            <span class="block text-sm text-gray-500 sm:text-center">©<a href="https://tahmidkhan.com/" class="transition-all duration-500 hover:text-white"> Tahmid Khan Arnab</a>. All Rights Reserved.
+            <span class="block text-sm text-gray-500 sm:text-center">©<a href="https://tahmidkhan.com/" class="transition-all duration-300 hover:text-white"> Tahmid Khan Arnab</a>. All Rights Reserved.
             </span>
         </footer>
 
