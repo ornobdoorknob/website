93a444c63a6cacfcd35e6266ac93a8617396def9
Tahmid Khan Arnab <arna0016@algonquinlive.com>
2023-03-09 12:01:10 -0500

    ver 12 aded patterns and button responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import Hero from './components/Hero';
 import Content from './components/Content';
 
 const App = () => (
-    <div class="min-h-screen w-full overflow-hidden bg-primary">
+  <div class="min-h-screen w-full overflow-hidden bg-primary bg-fixed">
+    <div class="z-0">
       <Hero />
+    </div>
+    <div class = "z-10">
       <Content />
       <div class="sticky top-[100vh]">
         <Footer />
       </div>
     </div>
+  </div>
 )
 
 export default App
